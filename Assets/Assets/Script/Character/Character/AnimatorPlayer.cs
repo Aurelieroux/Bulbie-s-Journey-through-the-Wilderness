117c4ce61a2c05170559ee9e30a85699204b6e6b
Aurélie Roux <aurelieemilieroux@outlook.fr>
2023-02-17 14:16:00 +0100

    correction déplacement, animation clean, ajout shop,ajout heart

---
+++ Assets/Assets/Script/Character/Character/AnimatorPlayer.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorPlayer : MonoBehaviour
+{
+    private Animator animator;
+
+    private bool walkRight;
+    private bool walkLeft;
+    private bool walkSouth;
+    private bool walkNorth;
+    private bool IdleFalse;
+
+    private bool isAttacking;
+
+    private Vector2 lastMove;
+    private bool swordLeft;
+    private bool swordRight;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        swordLeft = false;
+        swordRight = false;
+
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        walkRight = false;
+        walkLeft = false;
+        walkSouth = false;
+        walkNorth = false;
+        IdleFalse = true;
+
+        if (horizontal > 0)
+        {
+            walkRight = true;
+            IdleFalse = false;
+            lastMove = new Vector2(horizontal, 0f);
+        }
+        else if (horizontal < 0)
+        {
+            walkLeft = true;
+            IdleFalse = false;
+            lastMove = new Vector2(horizontal, 0f);
+        }
+
+        if (vertical > 0)
+        {
+            walkNorth = true;
+            IdleFalse = false;
+            lastMove = new Vector2(0f, vertical);
+        }
+        else if (vertical < 0)
+        {
+            walkSouth = true;
+            IdleFalse = false;
+            lastMove = new Vector2(0f, vertical);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("XButton")) // Si on appuie sur espace ou le bouton carré de la manette
+        {
+            isAttacking = true;
+
+            animator.SetTrigger("launchAttack");
+
+            if (lastMove.x > 0) // Si le personnage est orienté vers la droite
+            {
+                swordRight = true; // Définit la variable swordRight à true
+                swordLeft = false; // Définit la variable swordLeft à false
+            }
+            else if (lastMove.x < 0) // Si le personnage est orienté vers la gauche
+            {
+                swordLeft = true; // Définit la variable swordLeft à true
+                swordRight = false; // Définit la variable swordRight à false
+            }
+
+            if (walkRight)
+            {
+                animator.SetTrigger("launchAttackRight");
+            }
+            else if (walkLeft)
+            {
+                animator.SetTrigger("launchAttackLeft");
+            }
+            else if (walkNorth)
+            {
+                animator.SetTrigger("launchAttackNorth");
+            }
+            else if (walkSouth)
+            {
+                animator.SetTrigger("launchAttackSouth");
+            }
+            else
+            {
+                animator.SetTrigger("launchAttack");
+            }
+        }
+
+        animator.SetBool("walkRight", walkRight);
+        animator.SetBool("walkLeft", walkLeft);
+        animator.SetBool("walkSouth", walkSouth);
+        animator.SetBool("walkNorth", walkNorth);
+
+        if (isAttacking)
+        {
+            animator.SetLayerWeight(1, 1);
+        }
+        else
+        {
+            animator.SetLayerWeight(1, 0);
+        }
+    }
+
+    public void StartAttack()
+    {
+        // Do something when the attack starts, if necessary
+    }
+
+    public void EndAttack()
+    {
+        isAttacking = false;
+    }
+}
+
+
+
+
+
+
+
---
+++ Assets/Assets/Script/Character/Character/PlayerController.cs
@@ -0,0 +1,190 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private string walkRightAnimation = "Character_Walk_Right_Animation";
+    [SerializeField] private string walkLeftAnimation = "Character_Walk_Left_Animation";
+    [SerializeField] private string walkNorthAnimation = "Character_Walk_North_Animation";
+    [SerializeField] private string walkSouthAnimation = "Character_Walk_South_Animation";
+
+    [Header("Stats")]
+    [SerializeField] private float moveSpeed = 10.0f;
+    [SerializeField] private int maxHp = 20;
+    private int currentHp;
+
+    [Header("Attack")]
+    [SerializeField] private int attackDamage = 2;
+    [SerializeField] private float attackRange = 1.0f;
+
+    [Header("Respawn")]
+    [SerializeField] private Transform respawnPoint;
+
+    [Header("Sword")]
+    [SerializeField] private Transform swordNorth;
+    [SerializeField] private Transform swordSouth;
+    [SerializeField] private Transform swordLeft;
+    [SerializeField] private Transform swordRight;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private bool canMove = true;
+    private Vector2 lastMove = Vector2.zero;
+
+
+  
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentHp = maxHp;
+    }
+
+    private void Update()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHp -= damage;
+
+        if (currentHp <= 0)
+        {
+            Respawn();
+        }
+    }
+
+    private void Move()
+    {
+        float x = 0.0f;
+        float y = 0.0f;
+
+        // Déplacements au clavier
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            x += Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+            y += Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+            lastMove = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            if (lastMove.x > 0)
+            {
+                anim.SetBool("walkRight", true);
+                anim.SetBool("walkLeft", false);
+                anim.SetBool("walkNorth", false);
+                anim.SetBool("walkSouth", false);
+            }
+            else if (lastMove.x < 0)
+            {
+                anim.SetBool("walkRight", false);
+                anim.SetBool("walkLeft", true);
+                anim.SetBool("walkNorth", false);
+                anim.SetBool("walkSouth", false);
+            }
+            else if (lastMove.y > 0)
+            {
+                anim.SetBool("walkRight", false);
+                anim.SetBool("walkLeft", false);
+                anim.SetBool("walkNorth", true);
+                anim.SetBool("walkSouth", false);
+            }
+            else if (lastMove.y < 0)
+            {
+                anim.SetBool("walkRight", false);
+                anim.SetBool("walkLeft", false);
+                anim.SetBool("walkNorth", false);
+                anim.SetBool("walkSouth", true);
+            }
+        }
+        
+        else
+        {
+            anim.SetBool("walkRight", false);
+            anim.SetBool("walkLeft", false);
+            anim.SetBool("walkNorth", false);
+            anim.SetBool("walkSouth", false);
+        }
+
+        rb.velocity = new Vector2(x, y);
+
+    }
+
+
+        private void Attack()
+    {
+        Vector2 swordDirection = Vector2.zero;
+
+        if (lastMove.x > 0)
+        {
+            swordDirection = Vector2.right;
+        }
+        else if (lastMove.x < 0)
+        {
+            swordDirection = Vector2.left;
+        }
+        else if (lastMove.y > 0)
+        {
+            swordDirection = Vector2.up;
+        }
+        else if (lastMove.y < 0)
+        {
+            swordDirection = Vector2.down;
+        }
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position + (Vector3)swordDirection * attackRange, attackRange, LayerMask.GetMask("Enemies"));
+
+        if (hitEnemies != null)
+        {
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            }
+        }
+
+        // Animation de l'attaque
+        if (swordDirection == Vector2.up)
+        {
+            anim.SetTrigger("launchAttackNorth");
+        }
+        else if (swordDirection == Vector2.down)
+        {
+            anim.SetTrigger("launchAttackSouth");
+        }
+        else if (swordDirection == Vector2.right)
+        {
+            anim.SetTrigger("launchAttackRight");
+        }
+        else if (swordDirection == Vector2.left)
+        {
+            anim.SetTrigger("launchAttackLeft");
+        }
+    }
+
+    public void Respawn()
+    {
+        transform.position = respawnPoint.position;
+        currentHp = maxHp;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
